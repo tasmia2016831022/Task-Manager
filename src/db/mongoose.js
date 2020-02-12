096bf327608b3aac242d52223d6c1e8d223c5a1d
tasmia2016831022 <tasmia22@student.sust.edu>
2020-02-12 12:12:52 +0600

    mongoose modeling done

---
+++ src/db/mongoose.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api',{
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+})
+
+const User = mongoose.model('User', {
+    name: {
+        type: String
+    },
+    age: {
+        type: Number
+    }
+})
+
+const me = new User({
+    name: 'Minnni',
+    age: 23
+})
+
+me.save().then(() =>{
+    console.log(me);
+}).catch((error) => {
+    console.log("Error: ",error);
+})
