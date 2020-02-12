25ca000521b00e6ef35d8aec107325ed62b0cbbf
tasmia2016831022 <tasmia22@student.sust.edu>
2020-02-12 15:29:44 +0600

    task model created

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -5,16 +5,3 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api',{
     useCreateIndex: true,
     useUnifiedTopology: true
 })
-
-
-const Tasks = mongoose.model('Tasks', {
-    description: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    completed: {
-        type: Boolean,
-        default: false
-    }
-});
