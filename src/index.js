0a8674a2dfeb08f4ce20a6ea8d55b2caa193e824
tasmia2016831022 <tasmia22@student.sust.edu>
2020-02-12 16:36:07 +0600

    task read done

--- src/index.js
+++ src/index.js
@@ -28,6 +28,50 @@ app.post('/tasks',(req,res) => {
     })
 })
 
+app.get('/users',(req,res) => {
+    User.find().then((users) => {
+        res.send(users);
+    }).catch((error) => {
+        res.status(500).send(error);
+    })
+})
+
+app.get('/users/:id',(req,res) => {
+    const _id = req.params.id;
+    
+    User.findById(_id).then((user) => {
+        if(!user){
+            return res.status(404).send();
+        }
+        res.send(user);
+    }).catch((error) => {
+        res.status(500).send(error);
+    })
+})
+
+app.get('/tasks',(req,res) =>{
+    Task.find().then((tasks) => {
+        res.send(tasks);
+    }).catch((error) => {
+        res.status(400).send(error);
+    })
+})
+
+app.get('/tasks/:id',(req,res) => {
+    const _id = req.params.id;
+    
+    Task.findById(_id).then((task) => {
+        if(!task){
+            return res.status(404).send();
+        }
+        res.send(task);
+    }).catch((error) => {
+        res.status(500).send(error);
+    })
+})
+
+
+
 app.listen(port, () => {
     console.log(`Server started at port ${port}`);
 })
