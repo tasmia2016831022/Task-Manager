29c0568403a390ef0d43f856e192654b4a48783d
tasmia2016831022 <tasmia22@student.sust.edu>
2020-02-13 14:27:28 +0600

    user update done with validators

--- src/index.js
+++ src/index.js
@@ -10,6 +10,8 @@ app.use(express.json());
 
 ///===========USER API =================///
 
+///CREATE-GET///
+
 app.post("/users", async (req, res) => {
   const user = new User(req.body);
 
@@ -20,6 +22,8 @@ app.post("/users", async (req, res) => {
     res.status(400).send(error);
   }
 });
+
+///READ-POST///
 
 app.get("/users", async (req, res) => {
   try {
@@ -44,9 +48,39 @@ app.get("/users/:id", async (req, res) => {
   }
 });
 
+///UPDATE-PATCH///
+
+app.patch("/users/:id", async (req, res) => {
+    
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ['name', 'email', 'password', 'age'];
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
+    
+    if(!isValidOperation){
+        return res.status(400).send();
+    }
+    
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    });
+
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.send(user);
+    
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
 ///===========USER API =================///
 
 ///===========TASK API =================///
+
+///CREATE-GET///
 
 app.post("/tasks", async (req, res) => {
   const task = new Task(req.body);
@@ -58,6 +92,8 @@ app.post("/tasks", async (req, res) => {
     res.status(400).send(error);
   }
 });
+
+///READ-POST///
 
 app.get("/tasks", async (req, res) => {
   try {
