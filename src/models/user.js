eceef8756e5ac0951c07d72be8298716e42ed8b1
tasmia2016831022 <tasmia22@student.sust.edu>
2020-02-13 18:26:24 +0600

    fixing the middleware in user

--- src/models/user.js
+++ src/models/user.js
@@ -1,45 +1,52 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 
-
-
-const User = mongoose.model('User', {
-    name: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        lowercase:true,
-        validate(value){
-            if(!validator.isEmail(value)){
-                throw new Error(' Email is not valid');
+const userSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            trim: true,
+            lowercase:true,
+            validate(value){
+                if(!validator.isEmail(value)){
+                    throw new Error(' Email is not valid');
+                }
             }
-        }
-    },
-    password: {
-      type: String,
-      required: true,
-      trim: true,
-      minlength: 7,
-      validate(value){
-          if(validator.equals(value,'password') || validator.equals(value, 'Password')){
-              throw new Error(' password not valid ');
-          }
-      }  
-    },
-    age: {
-        type: Number,
-        default: 0,
-        validate(value){
-            if(value < 0){
-                throw new Error(' Age must be a positive number ');
+        },
+        password: {
+          type: String,
+          required: true,
+          trim: true,
+          minlength: 7,
+          validate(value){
+              if(validator.equals(value,'password') || validator.equals(value, 'Password')){
+                  throw new Error(' password not valid ');
+              }
+          }  
+        },
+        age: {
+            type: Number,
+            default: 0,
+            validate(value){
+                if(value < 0){
+                    throw new Error(' Age must be a positive number ');
+                }
             }
         }
     }
-});
+);
+
+userSchema.pre('save', async function(next){
+    const user = this;
+    next();
+})
+
+const User = mongoose.model('User', userSchema);
 
 module.exports = User;
--- src/router/userRouter.js
+++ src/router/userRouter.js
@@ -56,10 +56,11 @@ router.post("/users", async (req, res) => {
     }
   
     try {
-      const user = await User.findByIdAndUpdate(req.params.id, req.body, {
-        new: true,
-        runValidators: true
-      });
+      const user = await User.findByIdAndUpdate(req.params.id);
+      
+      updates.forEach((update) => user[update] = req.body[update]);
+      
+      await user.save();
   
       if (!user) {
         return res.status(404).send();
