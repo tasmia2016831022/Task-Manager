00e88aceb035ab14e7af5a3a27dc92726dfbc4ef
tasmia2016831022 <tasmia22@student.sust.edu>
2020-02-13 18:46:52 +0600

    task middleware done

--- src/router/taskRouter.js
+++ src/router/taskRouter.js
@@ -56,11 +56,12 @@ router.post("/tasks", async (req, res) => {
     }
   
     try {
-      const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
-        new: true,
-        runValidators: true
-      });
-  
+      const task = await Task.findByIdAndUpdate(req.params.id);
+      
+      updates.forEach((update) => task[update] = req.body[update]);
+      
+      await task.save();  
+      
       if (!task) {
         return res.status(404).send();
       }
