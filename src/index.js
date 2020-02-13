bf309e8f013c323e2461649742a456ad79353151
tasmia2016831022 <tasmia22@student.sust.edu>
2020-02-13 15:59:04 +0600

    task api delete handled

--- src/index.js
+++ src/index.js
@@ -76,6 +76,20 @@ app.patch("/users/:id", async (req, res) => {
   }
 });
 
+///DELETE-DELETE
+
+app.delete('/users/:id', async (req,res) =>{
+    try {
+        const user = await User.findByIdAndDelete(req.params.id);
+        if(!user){
+            return res.status(404).send()
+        }
+        res.send(user)
+    } catch (error) {
+        res.status(500).send();
+    }
+})
+
 ///===========USER API =================///
 
 ///===========TASK API =================///
@@ -145,6 +159,20 @@ app.patch("/tasks/:id", async (req, res) => {
     res.status(400).send(error);
   }
 });
+
+///DELETE-DELETE///
+
+app.delete('/tasks/:id', async (req,res) => {
+    try {
+        const task = await Task.findByIdAndDelete(req.params.id);
+        if(!task){
+            return res.status(404).send()
+        }
+        res.send(task);
+    } catch (error) {
+        res.status(500).send();
+    }
+})
 
 ///===========TASK API =================///
 
