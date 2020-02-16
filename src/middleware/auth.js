c3b872680f6d68c6803830aaff9d87c903e5ff91
tasmia2016831022 <tasmia22@student.sust.edu>
2020-02-16 17:24:40 +0600

    logout implemented

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -4,7 +4,7 @@ const User = require('../models/user');
 const auth = async (req, res, next) => {
   try {
       const token = req.header('Authorization').replace('Bearer ', '');
-      console.log(token);
+     // console.log(token);
       const decoded = jwt.verify(token, 'node');
     //   const decoded = jwt.verify(token, 'thisisnode');
        const user = await User.findOne({ _id: decoded._id, 'tokens.token': token })
@@ -13,11 +13,12 @@ const auth = async (req, res, next) => {
           throw new Error();
       }
       
+      req.token = token;
       req.user = user;
       next();
   } catch (error) {
-      res.status(401).send(error);
+      res.status(401).send({error: "please authenticate"});
   }
-}  
+}    
 
 module.exports = auth;
--- src/router/userRouter.js
+++ src/router/userRouter.js
@@ -30,6 +30,21 @@ router.post("/users", async (req, res) => {
     }
   })
   
+  ///LOGOUT-POST///
+  
+  router.post('/users/logout', auth, async (req,res) =>{
+    try {
+      req.user.tokens = req.user.tokens.filter((token) => {
+        return token.token !== req.token
+      })
+      await req.user.save();
+      
+      res.send();
+    } catch (error) {
+      res.status(500).send();
+    }
+  })
+  
   ///READ-GET///
   
   router.get("/users/me",auth, async (req, res) => {
