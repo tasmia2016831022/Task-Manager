ae942fa47a7a04d912bdd2156c928b41ad43f3ee
tasmia2016831022 <tasmia22@student.sust.edu>
2020-02-17 12:47:02 +0600

    delete all tasks of deleted user

--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken')
+const Task = require('../models/task');
 
 const userSchema = new mongoose.Schema(
     {
@@ -92,12 +93,22 @@ userSchema.statics.findByCredentials = async (email,password) =>{
     return user;
 }
 
+/// Hash the password before saving 
 userSchema.pre('save', async function(next){
     const user = this;
     
     if(user.isModified('password')){
         user.password = await bcrypt.hash(user.password, 8);
     }
+    next();
+})
+
+
+/// Delete user tasks when user is removed
+
+userSchema.pre('remove', async function(next) {
+    const user = this;
+    await Task.deleteMany({owner: user._id})
     next();
 })
 
