70281db9f68d9ce4d64119fce9bcbe9a1c74ea76
tasmia2016831022 <tasmia22@student.sust.edu>
2020-02-17 13:17:02 +0600

    filter data by completed value

--- src/router/taskRouter.js
+++ src/router/taskRouter.js
@@ -25,10 +25,19 @@ router.post("/tasks",auth, async (req, res) => {
   
   ///READ ALL TASKS OF AUTH USER-GET///
   
+  //GET /taks?completed=true
+  
   router.get("/tasks", auth, async (req, res) => {
+    const match ={};
+    if(req.query.completed){
+      match.completed = req.query.completed === 'true';
+    }
     try {
       //const tasks = await Task.find({owner: req.user._id});
-      await req.user.populate('tasks').execPopulate();
+      await req.user.populate({
+        path: 'tasks',
+        match
+      }).execPopulate();
       res.send(req.user.tasks);
     } catch (error) {
       res.status(400).send(error);
