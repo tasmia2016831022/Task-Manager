a218240ac001fcd8be64fac04429980f3f18b12c
tasmia2016831022 <tasmia22@student.sust.edu>
2020-02-17 12:06:52 +0600

    task according to auth

--- src/router/taskRouter.js
+++ src/router/taskRouter.js
@@ -34,11 +34,16 @@ router.post("/tasks",auth, async (req, res) => {
     }
   });
   
-  router.get("/tasks/:id", async (req, res) => {
+  ///READ-GET///
+  ///ONLY AUTH USER OF HIS OWN///
+  
+  router.get("/tasks/:id",auth, async (req, res) => {
     const _id = req.params.id;
   
     try {
-      const task = await Task.findById(_id);
+      // const task = await Task.findById(_id);
+      const task = await Task.findOne({_id,owner: req.user._id});
+      
       if (!task) {
         return res.status(404).send();
       }
